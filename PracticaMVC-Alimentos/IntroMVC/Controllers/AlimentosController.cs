b35fad4f8f5a7c99623e3ad0b14929e4c256392d
ailenaguino <ailenaguino@gmail.com>
2020-09-17 00:48:56 -0300

    Ya esta el ejercicio completo, falta manejar errorcillos

---
+++ PracticaMVC-Alimentos/IntroMVC/Controllers/AlimentosController.cs
@@ -0,0 +1,68 @@
+﻿using Entidades;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroMVC.Controllers
+{
+    public class AlimentosController : Controller
+    {
+        //Seguis acá? Escuchá Faun entonces
+
+        AlimentosServicio AlS = new AlimentosServicio();
+
+        public ActionResult Todos()
+        {
+            List<Alimento> alimentos = AlS.ListarAlimentos();
+
+            return View(alimentos);
+        }
+
+        public ActionResult Alta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Alta(FormCollection form)
+        {
+            Alimento a = new Alimento();
+            a.Nombre = form["Nombre"];
+            a.Peso = Int32.Parse(form["Peso"]);
+
+            AlS.CrearAlimento(a);
+
+            return RedirectToAction("Todos");
+        }
+
+        public ActionResult Editar(int id)
+        {
+            Alimento a = AlS.BuscarAlimentoPorId(id);
+
+            return View(a);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(FormCollection form)
+        {
+            Alimento a = new Alimento();
+            a.Nombre = form["Nombre"];
+            a.Peso = Int32.Parse(form["Peso"]);
+            a.Id = Int32.Parse(form["Id"]);
+
+            AlS.Editar(a);
+
+            return RedirectToAction("Todos");
+        }
+
+        public ActionResult Borrar(int id)
+        {
+            AlS.Eliminar(id);
+
+            return RedirectToAction("Todos");
+        }
+    }
+}
---
+++ PracticaMVC-Alimentos/Servicios/AlimentosServicio.cs
@@ -0,0 +1,72 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class AlimentosServicio
+    {
+        private static List<Alimento> alimentos { get; set; }
+
+        public AlimentosServicio()
+        {
+            if (alimentos == null)
+            {
+                alimentos = new List<Alimento>()
+                {
+                    new Alimento(){ Id=1, Nombre="A", Peso=50 },
+
+                };
+            }
+        }
+
+        public List<Alimento> ListarAlimentos()
+        {
+
+            return alimentos;
+        }
+
+        public void CrearAlimento(Alimento a)
+        {
+            int MaxID = 0;
+            if (alimentos.Count > 0) {
+            MaxID = alimentos.Max(b => b.Id);
+            }
+            MaxID++;
+
+            a.Id = MaxID;
+            alimentos.Add(a);
+        }
+
+        public void Editar(Alimento a)
+        {
+            Alimento alimentoEncontrado = this.BuscarAlimentoPorId(a.Id);
+            alimentoEncontrado.Nombre = a.Nombre;
+            alimentoEncontrado.Peso = a.Peso;
+        }
+
+        public Alimento BuscarAlimentoPorId(int id)
+        {
+            Alimento a = null;
+            foreach (Alimento aux in alimentos)
+            {
+                if (aux.Id == id)
+                {
+                    a = aux;
+                }
+            }
+
+            return a;
+        }
+
+        public void Eliminar(int id)
+        {
+            Alimento a = this.BuscarAlimentoPorId(id);
+            alimentos.Remove(a);
+        }
+
+    }
+}
